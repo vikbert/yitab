453a46f9c5696d685f1e3695570b0e3d29878458
Xun Zhou <segentor@gmail.com>
2020-05-27 21:04:40 +0200

    updates

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -2,6 +2,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import YiTab from './YiTab';
 
+document.addEventListener('DOMContentLoaded', () => {
+    window.onblur = function() {
+        document.getElementById('favicon').setAttribute('href', 'logo-fav.png');
+    };
+
+    window.onfocus = function() {
+        document.getElementById('favicon').setAttribute('href', 'logo.png');
+    };
+});
+
 chrome.tabs.query({active: true, currentWindow: true}, (tab) => {
     ReactDOM.render(<YiTab/>, document.getElementById('app-container'));
 });
